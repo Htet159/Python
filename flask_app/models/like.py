09c88983bb6aee1dfc840afcfa3708046e6f7a8e
Htet <htetmundt@gmail.com>
2022-04-27 12:23:04 -0700

    adding like functionality and edit project
    testing what caused the previous bug after commit

--- flask_app/models/like.py
+++
@@ -1,7 +0,0 @@
-from flask_app.config.mysqlconnection import connectToMySQL
-from flask import flash
-
-class Like:
-    def __init__(self, data):
-        self.db = "portfolio_model"
-        self.id = data['id']
